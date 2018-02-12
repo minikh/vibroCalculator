111673f3041e266cecd06fc85c468cd7733cfbcd
Your Name <you@example.com>
2018-02-13 02:21:00 +0300

    velocity

--- src/main/java/ru/minikh/vibrocalc/calc/VibroCalcByVelocity.java
+++ src/main/java/ru/minikh/vibrocalc/calc/VibroCalcByVelocity.java
@@ -3,7 +3,7 @@ package ru.minikh.vibrocalc.calc;
 import java.util.HashMap;
 import java.util.Map;
 
-public class VibroCalcByAcceleration {
+public class VibroCalcByVelocity {
 
     private final static Double _2_PI = 2 * Math.PI;
     private final static Double AVG_TO_RMS_KOEFF = 1.1098901098901098901098901098901;
@@ -12,14 +12,14 @@ public class VibroCalcByAcceleration {
 
     public Result calculate(Value value, Map<Parameter, EdIzm> parameters, Double freq) {
 
-        if (!(value.getParameter() == Parameter.A_g
-                || value.getParameter() == Parameter.A_db
-                || value.getParameter() == Parameter.A_mm_sec2
-                || value.getParameter() == Parameter.A_m_sec2)) {
+        if (!(value.getParameter() == Parameter.V_db_m_sec
+                || value.getParameter() == Parameter.V_db_mm_sec
+                || value.getParameter() == Parameter.V_m_sec
+                || value.getParameter() == Parameter.V_mm_sec)) {
             throw new RuntimeException("Не правильный параметр");
         }
 
-        Double accelerationRms = prepareValue(value);
+        Double velocityRms = prepareValue(value);
 
         Double _2piFreq = _2_PI * freq;
 
@@ -29,34 +29,34 @@ public class VibroCalcByAcceleration {
             Double result = null;
             switch (parameter.getKey()) {
                 case A_g:
-                    result = accelerationRms / G;
+                    result = (velocityRms * _2piFreq) / G / KILO;
                     break;
                 case A_m_sec2:
-                    result = accelerationRms;
+                    result = velocityRms * _2piFreq / KILO;
                     break;
                 case A_mm_sec2:
-                    result = accelerationRms * KILO;
+                    result = velocityRms * _2piFreq;
                     break;
                 case V_mm_sec:
-                    result = accelerationRms / _2piFreq * KILO;
+                    result = velocityRms;
                     break;
                 case V_m_sec:
-                    result = accelerationRms / _2piFreq;
+                    result = velocityRms / KILO;
                     break;
                 case D_m:
-                    result = accelerationRms / (_2piFreq) / (_2piFreq);
+                    result = velocityRms / (_2piFreq * KILO);
                     break;
                 case D_mm:
-                    result = accelerationRms / (_2piFreq / KILO) / (_2piFreq);
+                    result = velocityRms / _2piFreq;
                     break;
                 case A_db:
-                    result = 20.0 * Math.log10(accelerationRms / (G * Math.pow(10, -6)));
+                    result = 20.0 * Math.log10(velocityRms * _2piFreq / KILO / (G * Math.pow(10, -6)));
                     break;
                 case V_db_m_sec:
-                    result = 20.0 * Math.log10(accelerationRms / Math.pow(10, -8));
+                    result = 20.0 * Math.log10(velocityRms / KILO / Math.pow(10, -8));
                     break;
                 case V_db_mm_sec:
-                    result = 20.0 * Math.log10(accelerationRms / Math.pow(10, -6));
+                    result = 20.0 * Math.log10(velocityRms / Math.pow(10, -6));
                     break;
             }
 
@@ -90,13 +90,16 @@ public class VibroCalcByAcceleration {
         }
 
         switch (value.getParameter()) {
-            case A_g:
+            case V_db_m_sec:
                 rms *= G;
                 break;
-            case A_mm_sec2:
+            case V_db_mm_sec:
+                rms *= G;
+                break;
+            case V_m_sec:
                 rms /= KILO;
                 break;
-            case A_m_sec2:
+            case V_mm_sec:
                 break;
         }
         return rms;
---
+++ src/test/java/ru/minikh/vibrocalc/calc/VibroCalcVelocityTest.java
@@ -0,0 +1,275 @@
+package ru.minikh.vibrocalc.calc;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class VibroCalcVelocityTest {
+
+    @Test
+    public void testCalc1V() {
+        Value acceleration = Value.builder()
+                .value(1.0)
+                .edIzm(EdIzm.RMS)
+                .parameter(Parameter.V_mm_sec)
+                .build();
+
+        VibroCalcByVelocity vibroCalc = new VibroCalcByVelocity();
+
+        Map<Parameter, EdIzm> parameters = new HashMap<>();
+        parameters.put(Parameter.A_g, EdIzm.PEAK_TO_PEAK);
+        parameters.put(Parameter.A_m_sec2, EdIzm.RMS);
+        parameters.put(Parameter.A_mm_sec2, EdIzm.RMS);
+
+        parameters.put(Parameter.V_m_sec, EdIzm.RMS);
+        parameters.put(Parameter.V_mm_sec, EdIzm.RMS);
+        parameters.put(Parameter.D_m, EdIzm.PEAK_TO_PEAK);
+        parameters.put(Parameter.D_mm, EdIzm.PEAK_TO_PEAK);
+
+        parameters.put(Parameter.A_db, EdIzm.NONE);
+        parameters.put(Parameter.V_db_m_sec, EdIzm.NONE);
+        parameters.put(Parameter.V_db_mm_sec, EdIzm.NONE);
+
+        Result result = vibroCalc.calculate(acceleration, parameters, 1.0);
+
+        Value value = result.getValues().get(Parameter.V_mm_sec.name());
+        assertEquals(1.0, value.getValue(), 1.0e-004);
+
+        value = result.getValues().get(Parameter.V_m_sec.name());
+        assertEquals(0.001, value.getValue(), 1.0e-003);
+
+        value = result.getValues().get(Parameter.D_m.name());
+        assertEquals(0.000450158, value.getValue(), 1.0e-009);
+
+        value = result.getValues().get(Parameter.D_mm.name());
+        assertEquals(0.450158, value.getValue(), 1.0e-006);
+
+        value = result.getValues().get(Parameter.A_g.name());
+        assertEquals(0.00181218, value.getValue(), 1.0e-007);
+
+        value = result.getValues().get(Parameter.A_m_sec2.name());
+        assertEquals(0.00628319, value.getValue(), 1.0e-008);
+
+        value = result.getValues().get(Parameter.A_mm_sec2.name());
+        assertEquals(6.28319, value.getValue(), 1.0e-005);
+
+        value = result.getValues().get(Parameter.A_db.name());
+        assertEquals(56.1331, value.getValue(), 1.0e-004);
+
+        value = result.getValues().get(Parameter.V_db_m_sec.name());
+        assertEquals(100.0, value.getValue(), 1.0e-006);
+
+        value = result.getValues().get(Parameter.V_db_mm_sec.name());
+        assertEquals(120.0, value.getValue(), 1.0e-006);
+
+
+        for (Map.Entry<String, Value> valueEntry : result.getValues().entrySet()) {
+            System.out.println(valueEntry.getKey() + ": \t" + valueEntry.getValue().getValue());
+        }
+    }
+
+//    @Test
+    public void testCalc2A() {
+        Value acceleration = Value.builder()
+                .value(2.0)
+                .edIzm(EdIzm.RMS)
+                .parameter(Parameter.A_m_sec2)
+                .build();
+
+        VibroCalcByVelocity vibroCalc = new VibroCalcByVelocity();
+
+        Map<Parameter, EdIzm> parameters = new HashMap<>();
+        parameters.put(Parameter.A_g, EdIzm.PEAK_TO_PEAK);
+        parameters.put(Parameter.A_m_sec2, EdIzm.RMS);
+        parameters.put(Parameter.A_mm_sec2, EdIzm.RMS);
+
+        parameters.put(Parameter.V_m_sec, EdIzm.RMS);
+        parameters.put(Parameter.V_mm_sec, EdIzm.RMS);
+        parameters.put(Parameter.D_m, EdIzm.PEAK_TO_PEAK);
+        parameters.put(Parameter.D_mm, EdIzm.PEAK_TO_PEAK);
+
+        parameters.put(Parameter.A_db, EdIzm.NONE);
+        parameters.put(Parameter.V_db_m_sec, EdIzm.NONE);
+        parameters.put(Parameter.V_db_mm_sec, EdIzm.NONE);
+
+        Result result = vibroCalc.calculate(acceleration, parameters, 1.0);
+
+        Value value = result.getValues().get(Parameter.V_mm_sec.name());
+        assertEquals(318.31, value.getValue(), 0.001);
+
+        value = result.getValues().get(Parameter.V_m_sec.name());
+        assertEquals(0.31831, value.getValue(), 0.000001);
+
+        value = result.getValues().get(Parameter.D_m.name());
+        assertEquals(0.14329, value.getValue(), 0.000001);
+
+        value = result.getValues().get(Parameter.D_mm.name());
+        assertEquals(143.29, value.getValue(), 0.001);
+
+        value = result.getValues().get(Parameter.A_g.name());
+        assertEquals(0.576836, value.getValue(), 0.00001);
+
+        value = result.getValues().get(Parameter.A_m_sec2.name());
+        assertEquals(2.0, value.getValue(), 0.01);
+
+        value = result.getValues().get(Parameter.A_mm_sec2.name());
+        assertEquals(2000.0, value.getValue(), 0.01);
+
+        value = result.getValues().get(Parameter.A_db.name());
+        assertEquals(106.19, value.getValue(), 0.01);
+
+        value = result.getValues().get(Parameter.V_db_m_sec.name());
+//        assertEquals(170.057, value.getValue(), 0.0001);
+
+        value = result.getValues().get(Parameter.V_db_m_sec.name());
+//        assertEquals(170.057, value.getValue(), 0.0001);
+
+
+        for (Map.Entry<String, Value> valueEntry : result.getValues().entrySet()) {
+            System.out.println(valueEntry.getKey() + ": \t" + valueEntry.getValue().getValue());
+        }
+    }
+
+//    @Test
+    public void testCalc2A_mm_sec() {
+        Value acceleration = Value.builder()
+                .value(1.0)
+                .edIzm(EdIzm.RMS)
+                .parameter(Parameter.A_mm_sec2)
+                .build();
+
+        VibroCalcByVelocity vibroCalc = new VibroCalcByVelocity();
+
+        Map<Parameter, EdIzm> parameters = new HashMap<>();
+        parameters.put(Parameter.A_g, EdIzm.PEAK_TO_PEAK);
+        parameters.put(Parameter.A_m_sec2, EdIzm.RMS);
+        parameters.put(Parameter.A_mm_sec2, EdIzm.RMS);
+
+        parameters.put(Parameter.V_m_sec, EdIzm.RMS);
+        parameters.put(Parameter.V_mm_sec, EdIzm.RMS);
+        parameters.put(Parameter.D_m, EdIzm.PEAK_TO_PEAK);
+        parameters.put(Parameter.D_mm, EdIzm.PEAK_TO_PEAK);
+
+        parameters.put(Parameter.A_db, EdIzm.NONE);
+        parameters.put(Parameter.V_db_m_sec, EdIzm.NONE);
+        parameters.put(Parameter.V_db_mm_sec, EdIzm.NONE);
+
+        Result result = vibroCalc.calculate(acceleration, parameters, 1.0);
+
+        Value value = result.getValues().get(Parameter.V_mm_sec.name());
+        assertEquals(0.159155, value.getValue(), 0.000001);
+
+        value = result.getValues().get(Parameter.V_m_sec.name());
+        assertEquals(0.000159155, value.getValue(), 0.000000001);
+
+        value = result.getValues().get(Parameter.D_m.name());
+        assertEquals(7.16449e-005, value.getValue(), 0.000000001);
+
+        value = result.getValues().get(Parameter.D_mm.name());
+        assertEquals(0.0716449, value.getValue(), 0.000001);
+
+        value = result.getValues().get(Parameter.A_g.name());
+        assertEquals(0.000288418, value.getValue(), 0.00000001);
+
+        value = result.getValues().get(Parameter.A_m_sec2.name());
+        assertEquals(0.001, value.getValue(), 0.001);
+
+        value = result.getValues().get(Parameter.A_mm_sec2.name());
+        assertEquals(1.0, value.getValue(), 0.01);
+
+        value = result.getValues().get(Parameter.A_db.name());
+        assertEquals(40.1695, value.getValue(), 0.0001);
+
+        value = result.getValues().get(Parameter.V_db_m_sec.name());
+//        assertEquals(84.0364, value.getValue(), 0.0001);
+
+        value = result.getValues().get(Parameter.V_db_m_sec.name());
+//        assertEquals(104.036, value.getValue(), 0.0001);
+
+
+        for (Map.Entry<String, Value> valueEntry : result.getValues().entrySet()) {
+            System.out.println(valueEntry.getKey() + ": \t" + valueEntry.getValue().getValue());
+        }
+    }
+
+//    @Test
+    public void testCalc2Ag() {
+        Value acceleration = Value.builder()
+                .value(1.0)
+                .edIzm(EdIzm.PEAK_TO_PEAK)
+                .parameter(Parameter.A_g)
+                .build();
+
+        VibroCalcByVelocity vibroCalc = new VibroCalcByVelocity();
+
+        Map<Parameter, EdIzm> parameters = new HashMap<>();
+        parameters.put(Parameter.A_g, EdIzm.PEAK_TO_PEAK);
+        parameters.put(Parameter.A_m_sec2, EdIzm.RMS);
+        parameters.put(Parameter.A_mm_sec2, EdIzm.RMS);
+
+        parameters.put(Parameter.V_m_sec, EdIzm.RMS);
+        parameters.put(Parameter.V_mm_sec, EdIzm.RMS);
+        parameters.put(Parameter.D_m, EdIzm.PEAK_TO_PEAK);
+        parameters.put(Parameter.D_mm, EdIzm.PEAK_TO_PEAK);
+
+        parameters.put(Parameter.A_db, EdIzm.NONE);
+        parameters.put(Parameter.V_db_m_sec, EdIzm.NONE);
+        parameters.put(Parameter.V_db_mm_sec, EdIzm.NONE);
+
+        Result result = vibroCalc.calculate(acceleration, parameters, 1.0);
+
+        Value value = result.getValues().get(Parameter.V_mm_sec.name());
+        assertEquals(551.821, value.getValue(), 0.01);
+
+        value = result.getValues().get(Parameter.V_m_sec.name());
+        assertEquals(0.551821, value.getValue(), 1.0e-004);
+
+        value = result.getValues().get(Parameter.D_m.name());
+        assertEquals(0.248407, value.getValue(), 1.0e-005);
+
+        value = result.getValues().get(Parameter.D_mm.name());
+        assertEquals(248.407, value.getValue(), 1.0e-002);
+
+        value = result.getValues().get(Parameter.A_g.name());
+        assertEquals(1.0, value.getValue(), 0.00000001);
+
+        value = result.getValues().get(Parameter.A_m_sec2.name());
+        assertEquals(3.46719, value.getValue(), 1.0e-004);
+
+        value = result.getValues().get(Parameter.A_mm_sec2.name());
+        assertEquals(3467.19, value.getValue(), 1.0e-001);
+
+        value = result.getValues().get(Parameter.A_db.name());
+        assertEquals(110.969, value.getValue(), 1.0e-003);
+
+        value = result.getValues().get(Parameter.V_db_m_sec.name());
+//        assertEquals(154.836, value.getValue(), 0.0001);
+
+        value = result.getValues().get(Parameter.V_db_m_sec.name());
+//        assertEquals(174.836, value.getValue(), 0.0001);
+
+
+        for (Map.Entry<String, Value> valueEntry : result.getValues().entrySet()) {
+            System.out.println(valueEntry.getKey() + ": \t" + valueEntry.getValue().getValue());
+        }
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldException() {
+        Value acceleration = Value.builder()
+                .value(1.0)
+                .edIzm(EdIzm.PEAK_TO_PEAK)
+                .parameter(Parameter.A_g)
+                .build();
+
+        VibroCalcByVelocity vibroCalc = new VibroCalcByVelocity();
+
+        Map<Parameter, EdIzm> parameters = new HashMap<>();
+
+        vibroCalc.calculate(acceleration, parameters, 1.0);
+    }
+}
