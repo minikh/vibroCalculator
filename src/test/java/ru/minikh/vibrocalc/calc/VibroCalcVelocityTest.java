04f1be29a3293259836302ca0af3dcf69ee912ea
Your Name <you@example.com>
2018-02-14 01:03:06 +0300

    еще тест

--- src/test/java/ru/minikh/vibrocalc/calc/VibroCalcVelocityTest.java
+++ src/test/java/ru/minikh/vibrocalc/calc/VibroCalcVelocityTest.java
@@ -166,34 +166,82 @@ public class VibroCalcVelocityTest {
         Result result = vibroCalc.calculate(acceleration, 1.0);
 
         Value value = result.getValues().get(Parameter.V_mm_sec.name());
-        assertEquals(1.12202e-006, value.getValue(), 1.0e-004);
+        assertEquals(1.12202e-006, value.getValue(), 1.0e-0011);
 
         value = result.getValues().get(Parameter.V_m_sec.name());
-        assertEquals(1.12202e-009, value.getValue(), 1.0e-004);
+        assertEquals(1.12202e-009, value.getValue(), 1.0e-014);
 
         value = result.getValues().get(Parameter.D_m.name());
-        assertEquals(5.05086e-010, value.getValue(), 1.0e-005);
+        assertEquals(5.05086e-010, value.getValue(), 1.0e-0015);
 
         value = result.getValues().get(Parameter.D_mm.name());
-        assertEquals(5.05086e-007, value.getValue(), 1.0e-002);
+        assertEquals(5.05086e-007, value.getValue(), 1.0e-0012);
 
         value = result.getValues().get(Parameter.A_g.name());
-        assertEquals(2.0333e-009, value.getValue(), 1.0e-004);
+        assertEquals(2.0333e-009, value.getValue(), 1.0e-0013);
 
         value = result.getValues().get(Parameter.A_m_sec2.name());
-        assertEquals(7.04985e-009, value.getValue(), 1.0e-004);
+        assertEquals(7.04985e-009, value.getValue(), 1.0e-0014);
 
         value = result.getValues().get(Parameter.A_mm_sec2.name());
-        assertEquals(7.04985e-006, value.getValue(), 1.0e-001);
+        assertEquals(7.04985e-006, value.getValue(), 1.0e-0011);
 
         value = result.getValues().get(Parameter.A_db.name());
-        assertEquals(-62.8669, value.getValue(), 1.0e-003);
+        assertEquals(-62.8669, value.getValue(), 1.0e-004);
 
         value = result.getValues().get(Parameter.V_db_m_sec.name());
         assertEquals(-19, value.getValue(), 1.0e-004);
 
         value = result.getValues().get(Parameter.V_db_mm_sec.name());
         assertEquals(1, value.getValue(), 1.0e-004);
+
+
+        for (Map.Entry<String, Value> valueEntry : result.getValues().entrySet()) {
+            System.out.println(valueEntry.getKey() + ": \t" + valueEntry.getValue().getValue());
+        }
+    }
+
+    @Test
+    public void testCalc2VdbMSec() {
+        Value acceleration = Value.builder()
+                .value(1.0)
+                .edIzm(EdIzm.NONE)
+                .parameter(Parameter.V_db_m_sec)
+                .build();
+
+        VibroCalcByVelocity vibroCalc = new VibroCalcByVelocity();
+
+        Result result = vibroCalc.calculate(acceleration, 1.0);
+
+        Value value = result.getValues().get(Parameter.V_mm_sec.name());
+        assertEquals(1.12202e-005, value.getValue(), 1.0e-0010);
+
+        value = result.getValues().get(Parameter.V_m_sec.name());
+        assertEquals(1.12202e-008, value.getValue(), 1.0e-013);
+
+        value = result.getValues().get(Parameter.D_m.name());
+        assertEquals(5.05086e-009, value.getValue(), 1.0e-0014);
+
+        value = result.getValues().get(Parameter.D_mm.name());
+        assertEquals(5.05086e-006, value.getValue(), 1.0e-0011);
+
+        value = result.getValues().get(Parameter.A_g.name());
+        assertEquals(2.0333e-008, value.getValue(), 1.0e-0012);
+
+        value = result.getValues().get(Parameter.A_m_sec2.name());
+        assertEquals(7.04985e-008, value.getValue(), 1.0e-0013);
+
+        value = result.getValues().get(Parameter.A_mm_sec2.name());
+        assertEquals(7.04985e-005, value.getValue(), 1.0e-0010);
+
+        value = result.getValues().get(Parameter.A_db.name());
+        assertEquals(-42.8669, value.getValue(), 1.0e-004);
+
+        value = result.getValues().get(Parameter.V_db_m_sec.name());
+        assertEquals(1, value.getValue(), 1.0e-004);
+
+        value = result.getValues().get(Parameter.V_db_mm_sec.name());
+        assertEquals(21, value.getValue(), 1.0e-004);
 
 
         for (Map.Entry<String, Value> valueEntry : result.getValues().entrySet()) {
