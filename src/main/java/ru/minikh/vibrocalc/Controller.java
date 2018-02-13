5b9995a72ecc64ad1c076b96d06eadf13b4f6175
Your Name <you@example.com>
2018-02-13 13:21:11 +0300

    ui

--- src/main/java/ru/minikh/vibrocalc/Controller.java
+++ src/main/java/ru/minikh/vibrocalc/Controller.java
@@ -1,8 +1,29 @@
 package ru.minikh.vibrocalc;
 
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.input.KeyEvent;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public ComboBox adbSelectEdIzm;
+    public ComboBox vdbMmSecSelectEdIzm;
+
+    private final static String[] ED_IZM = {"СКЗ", "СЗ", "Пик", "Размах"};
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        adbSelectEdIzm.getItems().addAll(ED_IZM);
+        vdbMmSecSelectEdIzm.getItems().addAll(ED_IZM);
+
+
+        adbSelectEdIzm.setAccessibleText("СКЗ");
+        vdbMmSecSelectEdIzm.setAccessibleText("СКЗ");
+    }
+
     public void onEditA(KeyEvent keyEvent) {
 
     }
