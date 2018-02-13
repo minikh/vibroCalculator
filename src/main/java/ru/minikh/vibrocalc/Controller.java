0daa9c04fccf6e64153186145a93fa3990499ab2
Your Name <you@example.com>
2018-02-13 13:31:27 +0300

    ui

--- src/main/java/ru/minikh/vibrocalc/Controller.java
+++ src/main/java/ru/minikh/vibrocalc/Controller.java
@@ -9,19 +9,33 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    public ComboBox adbSelectEdIzm;
-    public ComboBox vdbMmSecSelectEdIzm;
-
     private final static String[] ED_IZM = {"СКЗ", "СЗ", "Пик", "Размах"};
+
+    public ComboBox accelerationGSelectEdIzm;
+    public ComboBox accelerationMsec2SelectEdIzm;
+    public ComboBox accelerationMmSec2SelectEdIzm;
+    public ComboBox velocityMsecSelectEdIzm;
+    public ComboBox velocityMmSecSelectEdIzm;
+    public ComboBox displacementMSelectEdIzm;
+    public ComboBox displacementMmSelectEdIzm;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        adbSelectEdIzm.getItems().addAll(ED_IZM);
-        vdbMmSecSelectEdIzm.getItems().addAll(ED_IZM);
+        accelerationGSelectEdIzm.getItems().addAll(ED_IZM);
+        accelerationMsec2SelectEdIzm.getItems().addAll(ED_IZM);
+        accelerationMmSec2SelectEdIzm.getItems().addAll(ED_IZM);
+        velocityMsecSelectEdIzm.getItems().addAll(ED_IZM);
+        velocityMmSecSelectEdIzm.getItems().addAll(ED_IZM);
+        displacementMSelectEdIzm.getItems().addAll(ED_IZM);
+        displacementMmSelectEdIzm.getItems().addAll(ED_IZM);
 
-
-        adbSelectEdIzm.setAccessibleText("СКЗ");
-        vdbMmSecSelectEdIzm.setAccessibleText("СКЗ");
+        accelerationGSelectEdIzm.getSelectionModel().select(3);
+        accelerationMsec2SelectEdIzm.getSelectionModel().select(0);
+        accelerationMmSec2SelectEdIzm.getSelectionModel().select(0);
+        velocityMsecSelectEdIzm.getSelectionModel().select(0);
+        velocityMmSecSelectEdIzm.getSelectionModel().select(0);
+        displacementMSelectEdIzm.getSelectionModel().select(3);
+        displacementMmSelectEdIzm.getSelectionModel().select(3);
     }
 
     public void onEditA(KeyEvent keyEvent) {
